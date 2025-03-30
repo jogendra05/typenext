204d3d8467e8399b3c7bdf3f36aa2e52a7abf48d
jogendra05 <jogendrasuthar03@gmail.com>
2025-03-30 22:19:40 +0530

    Add Hero Sectiom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center">
-        Hello World
+      <h1 className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+        <HeroSection />
       </h1>
     </div>
   );

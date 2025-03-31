5b2c4b18c0c913d3d9f6e0c89be180c9fa0277f8
jogendra05 <jogendrasuthar03@gmail.com>
2025-03-31 12:57:02 +0530

    Add Home Button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
 // import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center items-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
-      <Spotlight
-        className="-top-40 left-0 md:-top-20 md:left-60"
-        fill="white"
-      />
+        <Spotlight
+          className="-top-40 left-0 md:-top-20 md:left-60"
+          fill="white"
+        />
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master the art of music
         </h1>
@@ -19,15 +20,15 @@ const HeroSection = () => {
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
-                <Link href={"/courses"}>
-                    {/* <Button */}
-                    {/* borderRadius="1.75rem"
-                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                    > */}
-                    Explore courses
-                    {/* </Button> */}
-                </Link>
-            </div>
+          <Link href={"/courses"}>
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore courses
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );

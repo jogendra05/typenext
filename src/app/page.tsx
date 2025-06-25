121cefcc9943631e9692b449ebd6ad207aeb5041
jogendra05 <jogendrasuthar03@gmail.com>
2025-06-25 21:45:43 +0530

    Add Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCoureses from "@/components/FeaturedCoureses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructor";
 import MusicSchoolTestimonial from "@/components/MusicSchoolTestimonial";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -7,14 +9,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <div className="overflow-auto scrollbar-hide">
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection />
         <FeaturedCoureses/>
         <WhyChooseUs/>
         <MusicSchoolTestimonial/>
         <UpcomingWebinars/>
-      </h1>
+        <Instructors/>
+        <Footer/>
+      </main>
     </div>
   );
 }
